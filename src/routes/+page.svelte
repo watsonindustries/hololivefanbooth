937403634a65c35d39e30498cfa23a7bf64b8f46
Daniils Petrovs <thedanpetrov@gmail.com>
2023-02-13 22:44:50 +0100

    Add meta tags to root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,15 @@
 	import fanboothLogoLarge from '$lib/assets/fanbooth-logo-large.png';
 </script>
 
+<svelte:head>
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:creator" content="@danirukun" />
+	<meta property="og:url" content="https://www.hololivefanbooth.com" />
+	<meta property="og:title" content="Dokomi Hololive Fan Booth" />
+	<meta property="og:description" content="A Hololive Fan Booth by fans, for fans." />
+	<meta property="og:image" content={fanboothLogoLarge} />
+</svelte:head>
+
 <div class="hero min-h-screen bg-base-200">
 	<div class="hero-content text-center">
 		<div class="max-w-md space-y-3">
@@ -12,10 +21,8 @@
 			<p class="inline-block text-lg">at</p>
 			<p class="inline-block text-xl">Düsseldorf Congress Center South</p>
 
-			<p class="py-6 text-2xl font-bold">
-				Coming Soon!
-			</p>
-			<a href="https://www.dokomi.de/en" class="link link-primary">Dokomi.de</a>	
+			<p class="py-6 text-2xl font-bold">Coming Soon!</p>
+			<a href="https://www.dokomi.de/en" class="link link-primary">Dokomi.de</a>
 		</div>
 	</div>
 </div>
