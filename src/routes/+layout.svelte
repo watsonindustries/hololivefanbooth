1a8ec5772472a6dc68b33ff7a08d3de65d0b438e
Daniils Petrovs <daniils@platogo.com>
2023-05-12 20:04:39 +0200

    Update footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,10 +8,12 @@
   <div>
     <p>
       Copyright © 2023 - Powered by
-      <a href="https://watsonindustries.live" class="link">Watson Industries</a> - Not affiliated with Cover Corp
+      <a href="https://watsonindustries.live" class="link">Watson Industries</a> - Not affiliated with <a href="https://cover-corp.com" class="link">Cover Corp</a>
     </p>
     <p>
       <a href="/" class="mt-2 text-sm font-medium text-neutral-500 hover:text-neutral-900">Home</a>
+      <span class="text-neutral-500 mx-2">|</span>
+      <a href="/about" class="mt-2 text-sm font-medium text-neutral-500 hover:text-neutral-900">About</a>
       <span class="text-neutral-500 mx-2">|</span>
       <a href="/privacy" class="mt-2 text-sm font-medium text-neutral-500 hover:text-neutral-900">Privacy Policy</a>
     </p>
