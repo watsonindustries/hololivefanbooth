547c8f48b6e9c89743a1dfd75ad7ab9280355333
Daniils Petrovs <daniils@platogo.com>
2023-05-23 00:07:13 +0200

    Add missing meta tag

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,15 @@
+<footer class="footer footer-center p-4 bg-base-300 text-base-content">
+  <div>
+    <p>
+      Copyright © 2023 - Powered by
+      <a href="https://watsonindustries.live" class="link">Watson Industries</a> - Not affiliated with <a href="https://cover-corp.com" class="link">Cover Corp</a>
+    </p>
+    <p>
+      <a href="/" class="mt-2 text-sm font-medium text-neutral-400 hover:text-neutral-100 transition duration-400 ease-in-out">Home</a>
+      <span class="text-neutral-500 mx-2">|</span>
+      <a href="/about" class="mt-2 text-sm font-medium text-neutral-400 hover:text-neutral-100 transition duration-400 ease-in-out">About</a>
+      <span class="text-neutral-500 mx-2">|</span>
+      <a href="/privacy" class="mt-2 text-sm font-medium text-neutral-400 hover:text-neutral-100 transition duration-400 ease-in-out">Privacy Policy</a>
+    </p>
+  </div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,12 @@
 <script>
+	import Footer from '$lib/components/Footer.svelte';
 	import '../app.postcss';
 </script>
 
+<svelte:head>
+	<meta name="description" content="DoKomi 2023 Hololive fan booth project landing page.">
+</svelte:head>
+
 <slot />
 
-<footer class="footer footer-center p-4 bg-base-300 text-base-content">
-  <div>
-    <p>
-      Copyright © 2023 - Powered by
-      <a href="https://watsonindustries.live" class="link">Watson Industries</a> - Not affiliated with <a href="https://cover-corp.com" class="link">Cover Corp</a>
-    </p>
-    <p>
-      <a href="/" class="mt-2 text-sm font-medium text-neutral-400 hover:text-neutral-100 transition duration-400 ease-in-out">Home</a>
-      <span class="text-neutral-500 mx-2">|</span>
-      <a href="/about" class="mt-2 text-sm font-medium text-neutral-400 hover:text-neutral-100 transition duration-400 ease-in-out">About</a>
-      <span class="text-neutral-500 mx-2">|</span>
-      <a href="/privacy" class="mt-2 text-sm font-medium text-neutral-400 hover:text-neutral-100 transition duration-400 ease-in-out">Privacy Policy</a>
-    </p>
-  </div>
-</footer>
+<Footer />
