d322b55497e3a302e69f17be97f46e82dceb2fd6
Daniils Petrovs <daniils@platogo.com>
2023-05-23 00:41:48 +0200

    Add missing whitespace

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 <svelte:head>
 	<title>{title}</title>
 	<meta property="og:url" content="https://www.hololivefanbooth.com" />
-	<meta property="og:title" content={title + 'for DoKomi 2023'} />
+	<meta property="og:title" content={title + ' for DoKomi 2023'} />
 	<meta property="og:image" content={boothLogoLarge} />
 	<meta property="og:image:secure_url" content={boothLogoLarge} />
 	<meta property="og:description" content={description} />
@@ -18,7 +18,7 @@
 	<meta property="og:image:height" content="800" />
 
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content={title + 'for DoKomi 2023'} />
+	<meta name="twitter:title" content={title + ' for DoKomi 2023'} />
 	<meta name="twitter:description" content={description} />
 	<meta name="description" content="DoKomi 2023 Hololive fan booth project landing page.">
 </svelte:head>
