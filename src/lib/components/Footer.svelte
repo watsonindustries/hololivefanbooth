c28307de9979e792b399ee4b381be5e780ad8d57
Daniils Petrovs <daniils@platogo.com>
2023-05-31 00:18:42 +0200

    Add more breathing room to the footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,15 +1,28 @@
-<footer class="footer footer-center p-4 bg-base-100 text-base-content">
-  <div>
-    <p>
-      Copyright © 2023 - Powered by
-      <a href="https://watsonindustries.live" class="link">Watson Industries</a> - Not affiliated with <a href="https://cover-corp.com" class="link">Cover Corp</a>
-    </p>
-    <p>
-      <a href="/" class="mt-2 text-sm font-medium link link-secondary link-hover transition duration-400 ease-in-out">Home</a>
-      <span class="text-neutral-500 mx-2">|</span>
-      <a href="/about" class="mt-2 text-sm font-medium link link-secondary link-hover transition duration-400 ease-in-out">About</a>
-      <span class="text-neutral-500 mx-2">|</span>
-      <a href="/privacy" class="mt-2 text-sm font-medium link link-secondary link-hover transition duration-400 ease-in-out">Privacy Policy</a>
-    </p>
-  </div>
+<footer class="footer footer-center p-4 bg-base-100 text-base-content my-8">
+	<div class="space-y-8">
+		<p class="text-lg">
+			<a
+				href="/"
+				class="mt-2 font-medium link link-secondary link-hover transition duration-400 ease-in-out"
+				>Home</a
+			>
+			<span class="text-neutral-500 mx-2">|</span>
+			<a
+				href="/about"
+				class="mt-2 font-medium link link-secondary link-hover transition duration-400 ease-in-out"
+				>About</a
+			>
+			<span class="text-neutral-500 mx-2">|</span>
+			<a
+				href="/privacy"
+				class="mt-2 font-medium link link-secondary link-hover transition duration-400 ease-in-out"
+				>Privacy Policy</a
+			>
+		</p>
+		<p>
+			Copyright © 2023 - Powered by
+			<a href="https://watsonindustries.live" class="link">Watson Industries</a> - Not affiliated
+			with <a href="https://cover-corp.com" class="link">Cover Corp</a>
+		</p>
+	</div>
 </footer>
