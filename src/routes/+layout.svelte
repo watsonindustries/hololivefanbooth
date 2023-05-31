3c76ffbe0bee126bf11300f4157114bbc31ef4dc
Daniils Petrovs <daniils@platogo.com>
2023-05-31 20:06:56 +0200

    Add Svelte Meta Tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,25 +7,35 @@
 	import Navigation from '$lib/components/Navigation.svelte';
 	import type { LayoutData } from './$types';
 
+	import { MetaTags } from 'svelte-meta-tags';
+
 	export let data: LayoutData;
 </script>
 
-<Navigation></Navigation>
+<Navigation />
 
-<svelte:head>
-	<meta property="og:url" content="https://www.hololivefanbooth.com" />
-	<meta property="og:title" content={title + ' for DoKomi 2023'} />
-	<meta property="og:image" content={data.thumbnailImgURL} />
-	<meta property="og:image:secure_url" content={data.thumbnailImgURL} />
-	<meta property="og:description" content={description} />
-	<meta property="og:image:width" content="1200" />
-	<meta property="og:image:height" content="800" />
-
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content={title + ' for DoKomi 2023'} />
-	<meta name="twitter:description" content={description} />
-	<meta name="description" content="DoKomi 2023 Hololive fan booth project landing page.">
-</svelte:head>
+<MetaTags
+	openGraph={{
+		type: 'website',
+		url: 'https://www.hololivefanbooth.com',
+		title: title + ' for DoKomi 2023',
+		description: description,
+		images: [
+			{
+				url: data.thumbnailImgURL,
+				width: 1200,
+				height: 800,
+				alt: 'Hololive Fan Booth'
+			}
+		]
+	}}
+	additionalMetaTags={[
+		{
+			property: 'og:image:secure_url',
+			content: data.thumbnailImgURL
+		}
+	]}
+/>
 
 <slot />
 
