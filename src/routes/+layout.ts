bc598eb7981b6f9d9f81beb4ec739f6922625c3f
Daniils Petrovs <daniils@platogo.com>
2023-06-02 00:12:06 +0200

    Enable prerendering for all pages

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,6 +2,8 @@ import type { LayoutLoad } from './$types';
 
 import boothLogoLarge from '$lib/assets/fanbooth-logo-large.png';
 
+export const prerender = true;
+
 export const load = (async ({ url }) => {
 	const thumbnailImgURL = new URL(boothLogoLarge, url.origin).toString();
 
