bb2e84e97d79a8a3c00b5b4d2e5720d279b99477
Daniils Petrovs <daniils@platogo.com>
2023-06-04 20:18:08 +0200

    Add burger menu

---
+++ src/lib/components/Menu.svelte
@@ -0,0 +1,53 @@
+<script>
+	import { fly, slide } from 'svelte/transition';
+
+	export let open = false;
+
+	const links = [
+		{
+			name: 'Home',
+			href: '/'
+		},
+		{
+			name: 'About',
+			href: '/about'
+		},
+		{
+			name: 'Contact',
+			href: '/contact'
+		},
+		{
+			name: 'Support',
+			href: '/#support'
+		}
+	];
+</script>
+
+{#if open}
+	<div class="font-sans tracking-tighter" transition:slide={{duration: 1000}}>
+			{#each links as link, i}
+					<p transition:fly={{ y: -15, delay: 50 * i }}>
+							<a href="{link.href}" class="link link-secondary transition-colors">{link.name}</a>
+					</p>
+			{/each}
+	</div>
+{/if}
+
+<style>
+	div {
+			text-align: center;
+			font-size: 1.5em;
+			letter-spacing: 0.15em;
+			padding: 1em;
+			padding-top: 0;
+			color: #73dcff;
+	}
+	p {
+			cursor: pointer;
+			width: max-content;
+			margin: 1rem auto;
+	}
+	p:hover {
+			text-decoration: underline;
+	}
+</style>
--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,31 +1,14 @@
 <script lang="ts">
-	const tabs = [
-		{
-			name: 'Home',
-			href: '/'
-		},
-		{
-			name: 'About',
-			href: '/about'
-		},
-		{
-			name: 'Contact',
-			href: '/contact'
-		},
-		{
-			name: 'Support',
-			href: '/#support'
-		}
+	import {Hamburger} from 'svelte-hamburgers';
+	import Menu from './Menu.svelte';
 
-	];
+	let open = false;
 </script>
 
-<div class="navbar bg-base-100">
-	<div class="flex-1">
-		<ul class="menu menu-horizontal px-1 text-secondary font-semibold dark:text-primary">
-			{#each tabs as tab}
-				<li><a href={tab.href} class="hover:text-primary-focus">{tab.name}</a></li>
-			{/each}
-		</ul>
-	</div>
+<svelte:head />
+
+<div class="flex justify-end">
+	<Hamburger bind:open --color="#73dcff" />
 </div>
+
+<Menu bind:open />
