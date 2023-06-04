5af2a914980e6ecc09788d940a15c8db9287a6ff
Daniils Petrovs <daniils@platogo.com>
2023-06-04 19:42:39 +0200

    Update contact us page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
 	import { contactEmail, contactFormURL } from '../../const';
-	import type { PageData } from './$types';
 
-	export let data: PageData;
+	import { Twitter } from '@steeze-ui/simple-icons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+
+	const staffTwitter = [
+		{
+			handle: '@danirukun'
+		},
+		{
+			handle: '@Dominik_KFBeats'
+		}
+	];
 </script>
 
 <svelte:head>
@@ -15,8 +24,8 @@
 		<div
 			class="hero-content flex-col lg:flex-row-reverse bg-primary w-full sm:w-fit rounded-sm shadow-primary"
 		>
-			<div class="space-y-8 py-10 sm:p-48 text-secondary flex flex-col">
-				<h1 class="text-8xl text-center font-bold">Contact us!</h1>
+			<div class="space-y-8 py-10 sm:p-40 flex flex-col">
+				<h1 class="text-8xl text-center font-bold text-secondary">Contact us!</h1>
 				<a
 					href={contactFormURL}
 					class="btn btn-secondary btn-lg btn-outline max-w-xs mx-auto rounded-full transition duration-500"
@@ -29,6 +38,18 @@
 				>
 					Collaboration
 				</a>
+				<h2 class="text-6xl text-center font-bold text-secondary">Staff</h2>
+				<div class="mx-auto space-y-2">
+					{#each staffTwitter as { handle }}
+						<a
+							href={`https://twitter.com/${handle}`}
+							class="flex items-center gap-2 link link-hover link-secondary transition duration-500 justify-center"
+						>
+							<Icon src={Twitter} size="20px" />
+							{handle}
+						</a>
+					{/each}
+				</div>
 			</div>
 		</div>
 	</div>
