34a71db01d55a95b286f0ecf0d05ce660e0fed7c
Daniils Petrovs <daniils@platogo.com>
2024-05-05 22:26:12 +0200

    Add bubba demo

---
+++ src/routes/items/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { Canvas } from '@threlte/core';
+	import Scene from '$lib/components/Scene.svelte';
+
+	export let data: PageData;
+</script>
+
+<div class="h-screen">
+	<Canvas>
+		<Scene />
+	</Canvas>
+</div>
--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	ssr: {
+		noExternal: ['three']
+	},
+	server: {
+		fs: {
+			allow: ['static']
+		}
+	}
 });
