74408c2f79adb5c633feff4b2074b72dc5fce982
Daniils Petrovs <daniils@platogo.com>
2024-05-16 21:22:01 +0200

    Merch display improvements

--- src/routes/merch/+page.svelte
+++ src/routes/merch/+page.svelte
@@ -10,11 +10,18 @@
 	<meta name="description" content="Merch available at the booth for purchase, and as prizes" />
 </svelte:head>
 
-<section class="mx-auto mt-6 space-y-12 xl:max-w-5xl">
+<section class="mx-auto mt-6 space-y-12 px-6 xl:max-w-5xl">
 	<h1 class="text-center font-geologica text-8xl font-extrabold tracking-tighter text-primary">
 		Merch
 	</h1>
-	<div class="grid grid-cols-1 gap-4 px-2 sm:grid-cols-2 xl:grid-cols-3" id="merch-grid-container">
+	<section class="prose items-center text-xl">
+		<p class="font-geologica text-secondary">
+			Here you can find all the merch available at the booth for purchase, and as prizes.
+		</p>
+		<p class="font-geologica text-secondary">Click on the items to view more details.</p>
+	</section>
+
+	<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3" id="merch-grid-container">
 		<!-- Your content here -->
 		{#each data.merch as merch}
 			<MerchCard {merch} />
@@ -30,6 +37,7 @@
 					class="btn join-item"
 					class:btn-active={i + 1 == data.page}
 					class:btn-primary={i + 1 == data.page}
+					class:text-secondary-content={i + 1 == data.page}
 					href="/merch?page={i + 1}"
 					data-sveltekit-reload>{i + 1}</a>
 			{/each}
